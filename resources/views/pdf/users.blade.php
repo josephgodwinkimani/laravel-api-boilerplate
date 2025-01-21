335a3e1caf89f2ab63d89e54090f70d351eef447
Godwin Kimani <josephgodwinke@yandex.com>
2025-01-21 18:50:26 +0300

    add missing view for pdf export

---
+++ resources/views/pdf/users.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/pdf/users.blade.php -->
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
